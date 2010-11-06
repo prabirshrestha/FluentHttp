4bf886369c7832a45915001d1b9649f14716519f
prabirshrestha <mail@prabir.me>
2010-11-06 22:01:44 +0700

    + SpecFlow

---
+++ src/FluentHttp.Tests/HttpHeader/HttpHeaderCtor.cs
@@ -0,0 +1,43 @@
+﻿namespace FluentHttp.Tests.HttpHeader
+{
+    using TechTalk.SpecFlow;
+    using Xunit;
+
+    [Binding]
+    public class HttpHeaderCtor
+    {
+        private FluentHttpHeader fluentHttpHeader;
+        private string name;
+        private string value;
+
+        [Given(@"a new fluent http header with ctor params \(""(.*)"" and ""(.*)""\)")]
+        public void GivenANewFluentHttpHeaderWithCtorParamsHeader_NameAndHeader_Value(string headerName, string headerValue)
+        {
+            fluentHttpHeader = new FluentHttpHeader(headerName, headerValue);
+        }
+
+        [When(@"I get name")]
+        public void WhenIGetName()
+        {
+            name = fluentHttpHeader.Name;
+        }
+
+        [When(@"I get value")]
+        public void WhenIGetValue()
+        {
+            value = fluentHttpHeader.Value;
+        }
+
+        [Then(@"name should be ""(.*)""")]
+        public void ThenNameShouldBeHeader_Name(string headerName)
+        {
+            Assert.Equal(headerName, name);
+        }
+
+        [Then(@"value should be ""(.*)""")]
+        public void ThenValueShouldBeHeader_Value(string headerValue)
+        {
+            Assert.Equal(headerValue, value);
+        }
+    }
+}
---
+++ src/FluentHttp.Tests/HttpHeader/HttpHeaderCtor.feature
@@ -0,0 +1,10 @@
+﻿Feature: FluentHttpHeader ctor
+	Check if ctor assings value corrently
+
+@important
+Scenario: Pass header name and header value as ctor params
+	Given a new fluent http header with ctor params ("header-name" and "header-value")
+	When I get name
+	And I get value
+	Then name should be "header-name"
+	And value should be "header-value"
