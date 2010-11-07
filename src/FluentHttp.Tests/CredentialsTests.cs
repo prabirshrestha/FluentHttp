49ebae392e367ae956b4512366518c20811a2872
prabirshrestha <mail@prabir.me>
2010-11-07 23:50:18 +0700

    moved OAuth2 Authenticator to test project

---
+++ src/FluentHttp.Tests/CredentialsTests.cs
@@ -0,0 +1,19 @@
+namespace FluentHttp.Tests
+{
+    using System.Net;
+    using Xunit;
+
+    public class CredentialsTests
+    {
+        [Fact]
+        public void SetCredentialsTest()
+        {
+            var credentials = CredentialCache.DefaultCredentials;
+
+            var request = new FluentHttpRequest("http://prabir.me")
+                .Credentials(credentials);
+
+            Assert.Equal(credentials, request.GetCredentials());
+        }
+    }
+}
