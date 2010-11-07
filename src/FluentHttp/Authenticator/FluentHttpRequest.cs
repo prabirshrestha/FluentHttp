b8dff11ce0ff84845614d80b8bfbb052b23d14dd
prabirshrestha <mail@prabir.me>
2010-11-07 20:58:18 +0700

    + FluentAuthenticator

---
+++ src/FluentHttp/Authenticator/FluentHttpRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace FluentHttp
+{
+    using System.ComponentModel;
+
+    public partial class FluentHttpRequest
+    {
+        private IFluentAuthenticator _fluentAuthenticator;
+
+        /// <summary>
+        /// Sets the authenticator
+        /// </summary>
+        /// <param name="fluentAuthenticator">
+        /// The fluent authenticator.
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public FluentHttpRequest AuthenticateUsing(IFluentAuthenticator fluentAuthenticator)
+        {
+            _fluentAuthenticator = fluentAuthenticator;
+            return this;
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public IFluentAuthenticator GetAuthenticator()
+        {
+            return _fluentAuthenticator;
+        }
+    }
+}
