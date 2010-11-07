e5face71d01093209944668fa2a7dbe0580630fc
prabirshrestha <mail@prabir.me>
2010-11-07 23:26:26 +0700

    + Credentials fluent api

---
+++ src/FluentHttp/Credentials/FluentHttpRequest.cs
@@ -0,0 +1,37 @@
+﻿namespace FluentHttp
+{
+    using System.ComponentModel;
+    using System.Net;
+
+    public partial class FluentHttpRequest
+    {
+        private ICredentials _credentials;
+
+        /// <summary>
+        /// Set credentials
+        /// </summary>
+        /// <param name="credentials">
+        /// The credentials.
+        /// </param>
+        /// <returns>
+        /// FluentHttpRequest
+        /// </returns>
+        public FluentHttpRequest Credentials(ICredentials credentials)
+        {
+            _credentials = credentials;
+            return this;
+        }
+
+        /// <summary>
+        /// Gets the Credentials
+        /// </summary>
+        /// <returns>
+        /// Returns ICredentials
+        /// </returns>
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public ICredentials GetCredentials()
+        {
+            return _credentials;
+        }
+    }
+}
