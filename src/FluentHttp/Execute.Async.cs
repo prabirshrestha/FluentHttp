100c81a00d0bea1d33fb339353ca44524e324b00
prabirshrestha <mail@prabir.me>
2010-11-15 17:26:13 +0700

    + EndRequest

--- src/FluentHttp/Execute.Async.cs
+++ src/FluentHttp/Execute.Async.cs
@@ -10,6 +10,33 @@ namespace FluentHttp
     /// </summary>
     public partial class FluentHttpRequest
     {
-        
+        private FluentHttpAsyncResult _asyncResult;
+
+        public IAsyncResult BeginRequest(AsyncCallback callback,object state)
+        {
+            return null;
+        }
+
+        public FluentHttpResponse EndRequest(IAsyncResult asyncResult)
+        {
+            if (asyncResult == null)
+                throw new ArgumentNullException("asyncResult");
+
+            var ar = asyncResult as FluentHttpAsyncResult;
+
+            if (ar == null || !ReferenceEquals(_asyncResult, ar))
+                throw new ArgumentException("asyncResult");
+
+            // propagate the exception to the one who calls EndRequest.
+            if (ar.HttpRequestState.Exception != null)
+            {
+                ar.HttpRequestState.Response.ResponseStatus = ResponseStatus.Error;
+                throw ar.HttpRequestState.Exception;
+            }
+
+            ar.AsyncWaitHandle.WaitOne();
+
+            return ar.HttpRequestState.Response;
+        }
     }
 }
