fc6c7da15c01cde3d598a0e83284e67495d992f1
prabirshrestha <mail@prabir.me>
2010-12-10 22:45:34 +0700

    + contracts for Proxy

--- src/FluentHttp/Proxy/FluentHttpRequest.cs
+++ src/FluentHttp/Proxy/FluentHttpRequest.cs
@@ -1,22 +1,48 @@
 ﻿namespace FluentHttp
 {
     using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Diagnostics.Contracts;
     using System.Net;
 
+    [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1601:PartialElementsMustBeDocumented",
+        Justification = "Reviewed. Suppression is OK here.")]
     public partial class FluentHttpRequest
     {
-        private IWebProxy _proxy;
+        /// <summary>
+        /// Proxy to use when making http web request.
+        /// </summary>
+        private IWebProxy proxy;
 
+        /// <summary>
+        /// Sets the proxy.
+        /// </summary>
+        /// <param name="proxy">
+        /// The proxy to use.
+        /// </param>
+        /// <returns>
+        /// Returns <see cref="FluentHttpRequest"/>.
+        /// </returns>
+        [ContractVerification(true)]
         public FluentHttpRequest Proxy(IWebProxy proxy)
         {
-            _proxy = proxy;
+            Contract.Ensures(Contract.Result<FluentHttpRequest>() != null);
+
+            this.proxy = proxy;
+
             return this;
         }
 
+        /// <summary>
+        /// Gets the proxy.
+        /// </summary>
+        /// <returns>
+        /// Returns the proxy.
+        /// </returns>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public IWebProxy GetProxy()
         {
-            return _proxy;
+            return proxy;
         }
     }
 }
