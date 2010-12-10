7fb6fdc1e2fe8a3dba2b98f53899cb89a3dd26d2
prabirshrestha <mail@prabir.me>
2010-12-10 22:51:39 +0700

    - removed CopyStreamToStream from Utils

--- src/FluentHttp/Utils/Utils.cs
+++ src/FluentHttp/Utils/Utils.cs
@@ -1,10 +1,10 @@
-
 namespace FluentHttp
 {
-    using System;
-    using System.IO;
     using FluentHttp.External;
 
+    /// <summary>
+    /// Fluent Http Utilities
+    /// </summary>
     public class Utils
     {
         public static string UrlDecode(string input)
@@ -25,55 +25,6 @@ namespace FluentHttp
         public static string HtmlEncode(string input)
         {
             return HttpUtility.HtmlEncode(input);
-        }
-
-        public static void CopyStreamToStream(
-            Stream source, Stream destination,
-            Action<Stream, Stream, Exception> completed,
-            int bufferSize)
-        {
-            var buffer = new byte[bufferSize];
-
-            Action<Exception> done =
-                e =>
-                {
-                    if (completed != null)
-                        completed(source, destination, e);
-                };
-
-            AsyncCallback rc = null;
-            rc = readResult =>
-                     {
-                         try
-                         {
-                             int read = source.EndRead(readResult);
-                             if (read > 0)
-                             {
-                                 destination.BeginWrite(buffer, 0, read,
-                                                        writeResult =>
-                                                        {
-                                                            try
-                                                            {
-                                                                destination.EndWrite(writeResult);
-                                                                source.BeginRead(buffer, 0, buffer.Length, rc, null);
-                                                            }
-                                                            catch (Exception ex)
-                                                            {
-                                                                done(ex);
-                                                            }
-                                                        }, null);
-                             }
-                             else
-                             {
-                                 done(null);
-                             }
-                         }
-                         catch (Exception ex)
-                         {
-                             done(ex);
-                         }
-                     };
-            source.BeginRead(buffer, 0, buffer.Length, rc, null);
         }
     }
 }
