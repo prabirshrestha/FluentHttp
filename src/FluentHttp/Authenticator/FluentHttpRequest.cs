15c9eb1b82a8c1376ccb059c929efe580bad7cdd
prabirshrestha <mail@prabir.me>
2010-12-10 23:14:20 +0700

    + documentation for FluentAuthenticator

--- src/FluentHttp/Authenticator/FluentHttpRequest.cs
+++ src/FluentHttp/Authenticator/FluentHttpRequest.cs
@@ -1,29 +1,43 @@
 ﻿namespace FluentHttp
 {
     using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
 
+    [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1601:PartialElementsMustBeDocumented",
+        Justification = "Reviewed. Suppression is OK here.")]
     public partial class FluentHttpRequest
     {
-        private IFluentAuthenticator _fluentAuthenticator;
+        /// <summary>
+        /// Internal authenticator.
+        /// </summary>
+        private IFluentAuthenticator fluentAuthenticator;
 
         /// <summary>
-        /// Sets the authenticator
+        /// Sets the authenticator.
         /// </summary>
         /// <param name="fluentAuthenticator">
         /// The fluent authenticator.
         /// </param>
         /// <returns>
+        /// Returns <see cref="FluentHttpRequest"/>.
         /// </returns>
         public FluentHttpRequest AuthenticateUsing(IFluentAuthenticator fluentAuthenticator)
         {
-            _fluentAuthenticator = fluentAuthenticator;
+            this.fluentAuthenticator = fluentAuthenticator;
+
             return this;
         }
 
+        /// <summary>
+        /// Gets the authenticator.
+        /// </summary>
+        /// <returns>
+        /// Retruns <see cref="IFluentAuthenticator"/>.
+        /// </returns>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public IFluentAuthenticator GetAuthenticator()
         {
-            return _fluentAuthenticator;
+            return fluentAuthenticator;
         }
     }
 }
