7fb9fb4a4f92c9a28b59492b16c6e05364768e14
prabirshrestha <mail@prabir.me>
2010-12-12 13:48:15 +0700

    + added Cancelled for ResponseStatus enum + documented ResponseStatus enum

--- src/FluentHttp/ResponseStatus.cs
+++ src/FluentHttp/ResponseStatus.cs
@@ -1,11 +1,28 @@
 namespace FluentHttp
 {
+    /// <summary>
+    /// Response status
+    /// </summary>
     public enum ResponseStatus
     {
+        /// <summary>
+        /// Response not started.
+        /// </summary>
         Non,
 
+        /// <summary>
+        /// Web request completed successfully.
+        /// </summary>
         Completed,
 
-        Error
+        /// <summary>
+        /// Error occured executing web request.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Web request cancelled.
+        /// </summary>
+        Cancelled
     }
 }
