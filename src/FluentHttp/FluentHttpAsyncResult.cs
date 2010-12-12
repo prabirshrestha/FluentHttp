64b260e5e71319d998b58fad02c293a49dfc231a
prabirshrestha <mail@prabir.me>
2010-12-12 13:30:54 +0700

    + comments for FluentHttpAsyncResult

--- src/FluentHttp/FluentHttpAsyncResult.cs
+++ src/FluentHttp/FluentHttpAsyncResult.cs
@@ -1,58 +1,108 @@
 namespace FluentHttp
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Threading;
 
+    /// <summary>
+    /// Async Result for <see cref="FluentHttp"/>.
+    /// </summary>
     public class FluentHttpAsyncResult : IAsyncResult
     {
-        private AsyncCallback _callback;
-        private object _state;
+        /// <summary>
+        /// The callback.
+        /// </summary>
+        private readonly AsyncCallback callback;
 
-        private ManualResetEvent _waitHandle;
+        /// <summary>
+        /// Wait handle for the async result.
+        /// </summary>
+        private readonly ManualResetEvent waitHandle;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FluentHttpAsyncResult"/> class.
+        /// </summary>
+        /// <param name="callback">
+        /// The callback.
+        /// </param>
+        /// <param name="state">
+        /// The state.
+        /// </param>
         public FluentHttpAsyncResult(AsyncCallback callback, object state)
         {
-            _callback = callback;
-            _state = state;
-            _waitHandle = new ManualResetEvent(false);
+            this.callback = callback;
+            this.AsyncState = state;
+            this.waitHandle = new ManualResetEvent(false);
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the asynchronous operation has completed.
+        /// </summary>
+        /// <returns>
+        /// true if the operation is complete; otherwise, false.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
         public bool IsCompleted
         {
-            get { return _waitHandle.WaitOne(0, false); }
+            get { return this.waitHandle.WaitOne(0, false); }
         }
 
+        /// <summary>
+        /// Gets a <see cref="T:System.Threading.WaitHandle"/> that is used to wait for an asynchronous operation to complete.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Threading.WaitHandle"/> that is used to wait for an asynchronous operation to complete.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
         public WaitHandle AsyncWaitHandle
         {
-            get { return _waitHandle; }
+            get { return this.waitHandle; }
         }
 
-        public object AsyncState
-        {
-            get { return _state; }
-            protected internal set { _state = value; }
-        }
+        /// <summary>
+        /// Gets a user-defined object that qualifies or contains information about an asynchronous operation.
+        /// </summary>
+        /// <returns>
+        /// A user-defined object that qualifies or contains information about an asynchronous operation.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules",
+            "SA1623:PropertySummaryDocumentationMustMatchAccessors", Justification = "Reviewed. Suppression is OK here.")]
+        public object AsyncState { get; protected internal set; }
 
+        /// <summary>
+        /// Gets a value indicating whether the asynchronous operation completed synchronously.
+        /// </summary>
+        /// <returns>
+        /// true if the asynchronous operation completed synchronously; otherwise, false.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
         public bool CompletedSynchronously
         {
             get { return false; }
         }
 
+        /// <summary>
+        /// Gets or sets the <see cref="HttpRequestState"/>.
+        /// </summary>
+        internal HttpRequestState HttpRequestState { get; set; }
+
+        /// <summary>
+        /// Signal the asynchronous operation has completed.
+        /// </summary>
         internal void Complete()
         {
-            _waitHandle.Set();
+            this.waitHandle.Set();
 
-            if (HttpRequestState != null)
+            if (this.HttpRequestState != null)
             {
                 // don't set the http request state to null.
                 // EndRequest method still needs to access it.
-                HttpRequestState.Dispose();
+                this.HttpRequestState.Dispose();
             }
 
-            if (_callback != null)
-                _callback(this);
+            if (this.callback != null)
+                this.callback(this);
         }
-
-        internal HttpRequestState HttpRequestState { get; set; }
     }
 }
