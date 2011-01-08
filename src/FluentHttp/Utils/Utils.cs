08f5d291d0794dbb112338039056a93c0a0d9eeb
prabirshrestha <mail@prabir.me>
2011-01-08 18:43:29 +0700

    + Utils.Merge method to merge IDictionary<string,object>

--- src/FluentHttp/Utils/Utils.cs
+++ src/FluentHttp/Utils/Utils.cs
@@ -1,5 +1,7 @@
 namespace FluentHttp
 {
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
     using FluentHttp.External;
 
     /// <summary>
@@ -25,6 +27,36 @@ namespace FluentHttp
         public static string HtmlEncode(string input)
         {
             return HttpUtility.HtmlEncode(input);
+        }
+
+        /// <summary>
+        /// Merges two dictionaries.
+        /// </summary>
+        /// <param name="first">Default values, only used if second does not contain a value.</param>
+        /// <param name="second">Every value of the merged object is used.</param>
+        /// <returns>The merged dictionary</returns>
+        public static IDictionary<string, object> Merge(IDictionary<string, object> first, IDictionary<string, object> second)
+        {
+            Contract.Ensures(Contract.Result<IDictionary<string, object>>() != null);
+
+            first = first ?? new Dictionary<string, object>();
+            second = second ?? new Dictionary<string, object>();
+            var merged = new Dictionary<string, object>();
+
+            foreach (var kvp in second)
+            {
+                merged.Add(kvp.Key, kvp.Value);
+            }
+
+            foreach (var kvp in first)
+            {
+                if (!merged.ContainsKey(kvp.Key))
+                {
+                    merged.Add(kvp.Key, kvp.Value);
+                }
+            }
+
+            return merged;
         }
     }
 }
