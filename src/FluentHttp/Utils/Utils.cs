b88b3bdc72575dadf7d99626f3eb191e04b63118
prabirshrestha <mail@prabir.me>
2011-02-01 23:45:59 +0700

    - removed Merging of IDictionary<string, object>

--- src/FluentHttp/Utils/Utils.cs
+++ src/FluentHttp/Utils/Utils.cs
@@ -153,36 +153,6 @@ namespace FluentHttp
         /// <param name="first">Default values, only used if second does not contain a value.</param>
         /// <param name="second">Every value of the merged object is used.</param>
         /// <returns>The merged dictionary</returns>
-        public static IDictionary<string, object> Merge(IDictionary<string, object> first, IDictionary<string, object> second)
-        {
-            Contract.Ensures(Contract.Result<IDictionary<string, object>>() != null);
-
-            first = first ?? new Dictionary<string, object>();
-            second = second ?? new Dictionary<string, object>();
-            var merged = new Dictionary<string, object>();
-
-            foreach (var kvp in second)
-            {
-                merged.Add(kvp.Key, kvp.Value);
-            }
-
-            foreach (var kvp in first)
-            {
-                if (!merged.ContainsKey(kvp.Key))
-                {
-                    merged.Add(kvp.Key, kvp.Value);
-                }
-            }
-
-            return merged;
-        }
-
-        /// <summary>
-        /// Merges two dictionaries.
-        /// </summary>
-        /// <param name="first">Default values, only used if second does not contain a value.</param>
-        /// <param name="second">Every value of the merged object is used.</param>
-        /// <returns>The merged dictionary</returns>
         public static IDictionary<TKey, TValue> Merge<TKey, TValue>(IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second)
         {
             Contract.Ensures(Contract.Result<IDictionary<string, object>>() != null);
